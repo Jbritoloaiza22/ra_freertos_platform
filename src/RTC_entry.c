70d5445e6ab2388c9ccce75fb2eda792cbfc6345
Jbritoloaiza22 <britoloaizajd33@hotmail.com>
2026-06-05 17:48:34 -0600

    Add RTC driver

---
+++ src/RTC_entry.c
@@ -0,0 +1,13 @@
+#include "RTC.h"
+/* RTC_interface entry function */
+/* pvParameters contains TaskHandle_t */
+void RTC_entry(void *pvParameters)
+{
+    FSP_PARAMETER_NOT_USED (pvParameters);
+
+    /* TODO: add your own code here */
+    while (1)
+    {
+        vTaskDelay (1);
+    }
+}
