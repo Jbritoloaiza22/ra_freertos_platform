277fd625060da38764c6b1965293cc21a55407be
Jbritoloaiza22 <britoloaizajd33@hotmail.com>
2026-06-06 22:03:15 -0600

    Adding anomaly task with fsp

---
+++ src/ANOMALY_entry.c
@@ -0,0 +1,13 @@
+#include "ANOMALY.h"
+/* ANOMALY_task entry function */
+/* pvParameters contains TaskHandle_t */
+void ANOMALY_entry(void *pvParameters)
+{
+    FSP_PARAMETER_NOT_USED (pvParameters);
+
+    /* TODO: add your own code here */
+    while (1)
+    {
+        vTaskDelay (1);
+    }
+}
